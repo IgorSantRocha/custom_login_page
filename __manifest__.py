8b36b2c4b27e4c993c7be767be773a03be2c67cc
IgorSantRocha <igor77876@gmail.com>
2024-05-14 15:18:54 -0300

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Kolpolok Ltd. (https://www.kolpolok.com)
+#    Author: Kolpolok (<https://www.kolpolok.com>)
+#
+################################################################################
+
+{
+    'name': "Custom Login Page",
+    'version': '16.0.1.0',
+    'summary': """This module assists in configuring a background image for the login page and hide other odoo elements""",
+    'description': """Module helps to set background image in Login page.""",
+    'license': 'LGPL-3',
+    'website': "https://www.kolpoloktechnologies.com",
+    'author': 'Kolpolok',
+    'category': 'Tools',
+    'depends': ['base', 'portal', 'web'],
+    'data': [
+        'views/res_company.xml',
+        'views/web_login.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'sequence': 1,
+    "application": True,
+    "installable": True
+}
